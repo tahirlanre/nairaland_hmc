ea01475d289310c5476ab1decb887a94bc252a1a
tahirlanre <tahirlanre@hotmail.com>
2020-02-07 20:47:56 +0000

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Scraping nairaland for Public Health topics',
+    author='Tahir',
+    license='',
+)
